35e31ba0e42e6a6f148ff4e616ae0c6edbec65fd
Abdurakhmanov Daniyel <daniel.kapchagay@gmail.com>
2025-04-17 22:35:17 +0500

    check 4

---
+++ code_sample.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, m, k;
+    cin >> n >> m >> k;
+    cout << n << " " << m << " " << k << "\n";
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    cout << 0 << " ";
+    return 0;
+}
